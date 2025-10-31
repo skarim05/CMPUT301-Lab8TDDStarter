6905ff26c813dd5ab47efe3d1787130f2b672c5d
suhay <suhayb@ualberta.ca>
2025-10-31 08:59:02 -0600

    passing countCity test

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -22,6 +22,10 @@ public class CustomList {
         cities.remove(city);
     }
 
+    public int countCity(){
+        return cities.size();
+
+    }
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
 }
